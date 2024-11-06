ff5860729e212f75736467b1965cfa42930a88e2
Chris Adkin <chris@exadat.co.uk>
2024-11-06 18:41:14 +0000

    Files added to run hashicups sample app

--- tf/prometheus.tf
+++ tf/prometheus.tf
@@ -89,10 +89,11 @@ EOF
 resource "null_resource" "autoscaler" {
   provisioner "local-exec" {
     command = "export NOMAD_ADDR=$NA && nomad-pack list && nomad-pack run nomad_autoscaler --registry=community --parser-v1"
+  
+    environment = {
+        NA = "http://${module.nomad.fqdn}:4646"
+    }
   }
-  environment = {
-      NA = "http://${module.nomad.fqdn}:4646"
-  }
-
+  
   depends_on = [ nomad_job.prometheus ]
 }
